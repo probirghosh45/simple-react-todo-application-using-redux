5996fd9978b98096ceb8a3188a4371fabf65edc2
probir ghosh <probirghosh@outlook.com>
2022-08-21 11:08:06 +0600

    actions & actionTypes added for filter

--- src/redux/todos/actionTypes.js
+++ src/redux/todos/actionTypes.js
@@ -1,6 +1,6 @@
 export const ADDED = "todos/added";
-export const ALLCOMPLETED ="todos/allcompleted";
-export const CANCELCOMPLETED ="todos/cancelcompleted";
+export const ALLCOMPLETED ="todos/allCompleted";
+export const CANCELCOMPLETED ="todos/cancelCompleted";
 export const TOGGLED = "todos/toggled";
-export const COLORSELECTED ="todos/colorselected";
+export const COLORSELECTED ="todos/colorSelected";
 export const DELETED ="todos/deleted";
