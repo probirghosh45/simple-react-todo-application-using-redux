70051c127ca01320df188218c5080ef22eb248fb
probir ghosh <probirghosh@outlook.com>
2022-08-22 19:17:38 +0600

    color change issue fixed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import cancel from "../../src/images/cancel.png";
-import { toggled } from "../redux/todos/actions";
+import { colorSelected, toggled } from "../redux/todos/actions";
 
 const Todo = ({ todo }) => {
   // console.log(todo)
@@ -11,6 +11,9 @@ const handleStatusChange = (todoId) =>{
   dispatch(toggled(todoId))
 }
 
+const handleColorChange = (todoId,color) =>{
+  dispatch(colorSelected(todoId,color))
+}
 
   return (
     <div class="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
@@ -39,16 +42,19 @@ const handleStatusChange = (todoId) =>{
         class={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500  hover:bg-green-500 ${
           color === "green" && "bg-green-500"
         }`}
-      ></div>
+        onClick={()=>handleColorChange(id,"green")}
+        ></div>
       <div
         class={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500  ${
           color === "yellow" && "bg-yellow-500"
         }`}
+        onClick={()=>handleColorChange(id,"yellow")}
       ></div>
       <div
         class={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500  ${
           color === "red" && "bg-red-500"
         }`}
+        onClick={()=>handleColorChange(id,"red")}
       ></div>
 
       <img
