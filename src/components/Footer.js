35047318c1e01647f0d8089ba65f9878fe494471
probir ghosh <probirghosh@outlook.com>
2022-08-22 20:57:09 +0600

    color change task status issue fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { statusChanged } from "../redux/filters/actions";
+import { colorChanged, statusChanged } from "../redux/filters/actions";
 
 const Footer = () => {
   const todos = useSelector((state) => state.todos);
@@ -27,6 +27,15 @@ const Footer = () => {
   const handleStatusChange = (status) => {
     dispatch(statusChanged(status));
   };
+
+  const handleColorChange = (color) =>{
+   if(colors.includes(color)){
+    dispatch(colorChanged(color,"removed"))
+   }
+   else{
+    dispatch(colorChanged(color,"added"))
+   }
+  }
   
   return (
     <div className="mt-4 flex justify-between text-xs text-gray-500">
@@ -54,9 +63,15 @@ const Footer = () => {
         </li>
         <li></li>
         <li></li>
-        <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
-        <li className="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
-        <li className="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
+        <li className={`h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer ${colors.includes("green") && "bg-green-500"}`}
+        onClick={()=>handleColorChange("green")}
+        ></li>
+        <li className={`h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer ${colors.includes("red") && "bg-red-500"}`}
+        onClick={()=>handleColorChange("red")}
+        ></li>
+        <li className={`h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer ${colors.includes("yellow") && "bg-yellow-500" }`}
+        onClick={()=>handleColorChange("yellow")}
+        ></li>
       </ul>
     </div>
   );
