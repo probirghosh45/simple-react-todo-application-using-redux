0e905aa0dc6ac01dae336735a7a7df29878ff860
probir ghosh <probirghosh@outlook.com>
2022-08-23 18:19:43 +0600

    completed status & colores issue fixed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import Todo from './Todo';
-import { useSelector } from 'react-redux'
+import React from "react";
+import Todo from "./Todo";
+import { useSelector } from "react-redux";
 const TodoList = () => {
-  const todos = useSelector((state)=>state.todos)
-  console.log(todos);  
+  const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  console.log(todos);
   return (
-        <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-        {
-          todos.map((todo)=> <Todo  todo={todo}  key={todo.id} />)
-        }
-      </div>
-    );
+    <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+      {todos
+        .filter((todo) => {
+          const { status } = filters;
+          switch (status) {
+            case "Complete":
+              return todo.completed;
+            case "Incomplete":
+              return !todo.completed;
+            default:
+              return true;
+          }
+        })
+        .filter((todo)=>{
+          const {colors} = filters;
+          if(colors.length > 0 ){
+             return colors.includes(todo?.color)
+          }
+          return true; 
+        })
+        .map((todo) => (
+          <Todo todo={todo} key={todo.id} />
+        ))}
+    </div>
+  );
 };
 
 export default TodoList;
