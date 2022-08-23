6618b6f285bb71590ff2ae1aeb3b62c87fa32b31
probir ghosh <probirghosh@outlook.com>
2022-08-23 19:31:14 +0600

    initial commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,28 +4,33 @@ import { useSelector } from "react-redux";
 const TodoList = () => {
   const todos = useSelector((state) => state.todos);
   const filters = useSelector((state) => state.filters);
-  console.log(todos);
+  // console.log(todos);
+
+  const filterByStatus = (todo) => {
+    const { status } = filters;
+    switch (status) {
+      case "Complete":
+        return todo.completed;
+      case "Incomplete":
+        return !todo.completed;
+      default:
+        return true;
+    }
+  }
+
+  const filterByColor =(todo)=>{
+    const {colors} = filters;
+    if(colors.length > 0 ){
+       return colors.includes(todo?.color)
+    }
+    return true;  
+  }
+  
   return (
     <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {todos
-        .filter((todo) => {
-          const { status } = filters;
-          switch (status) {
-            case "Complete":
-              return todo.completed;
-            case "Incomplete":
-              return !todo.completed;
-            default:
-              return true;
-          }
-        })
-        .filter((todo)=>{
-          const {colors} = filters;
-          if(colors.length > 0 ){
-             return colors.includes(todo?.color)
-          }
-          return true;  
-        })
+        .filter(filterByStatus)
+        .filter(filterByColor)
         .map((todo) => (
           <Todo todo={todo} key={todo.id} />
         ))}
