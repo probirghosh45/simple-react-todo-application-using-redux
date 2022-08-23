9187513b9cc79ec81e5365df80e3bdd16cdfc669
probir ghosh <probirghosh@outlook.com>
2022-08-23 17:30:16 +0600

    added task issue fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,39 @@
-import React from "react";
-import doubleTick from '../../src/images/double-tick.png'
-import notes from '../../src/images/notes.png'
-import plusImage from '../../src/images/plus.png'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import doubleTick from "../../src/images/double-tick.png";
+import notes from "../../src/images/notes.png";
+import plusImage from "../../src/images/plus.png";
+import { added } from "../redux/todos/actions";
 
 const Header = () => {
+  const [input, setInput] = useState("");
+  console.log(input);
+  const dispatch = useDispatch()
+
+  const handleInput = (e) => {
+    // console.log(e.target.value)
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(added(input))
+    setInput("")
+  };
+
   return (
     <div>
-      <form class="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+      <form
+        class="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+        onSubmit={handleSubmit}
+      >
         <img src={notes} class="w-6 h-6" alt="Add todo" />
         <input
           type="text"
           placeholder="Type your todo"
           class="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+          value={input}
+          onChange={handleInput}
         />
         <button
           type="submit"
