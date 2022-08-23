fbc7a71879fbecda54969b78097fa5ff983cec77
probir ghosh <probirghosh@outlook.com>
2022-08-23 18:24:44 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import store from "./redux/store";
 
 function App() {
-  return (
+  return ( 
     <Provider store={store} >
       <div class="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
         <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
